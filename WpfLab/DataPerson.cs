b1466c656957afa8a46d8727628bd161f97e3dc1
Николай Афанасенко <kolyaafanasenko2@gmail.com>
2022-04-14 17:27:04 +0300

    Добавьте файлы проекта.

---
+++ WpfLab/DataPerson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLab
+{
+    public class DataPerson
+    {
+        public DateTime Date { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public DataPerson(DateTime date, string name, string surname, string patronymic, string city, string country)
+        {
+            Date = date;
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            City = city;
+            Country = country;
+        }
+    }
+}
